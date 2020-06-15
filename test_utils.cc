a038a21446bd2e4a30623ab45d6d32a0cbddec1a
yewenlyu2019 <46354696+yewenlyu2019@users.noreply.github.com>
2020-06-15 10:30:23 -0700

    intergrate template type check to improve simplicity

--- test_utils.cc
+++ test_utils.cc
@@ -1,18 +1,18 @@
 #include "test_utils.h"
 
 #include <ctype.h>
+#include <type_traits>
 
 namespace test_utils
 {
     template <class T>
-    std::vector<T> str_to_vector(std::string input_string, char t)
+    std::vector<T> str_to_vector(std::string input_string)
     {
         std::vector<T> output_vector;
 
-        switch (t)
-        {
         // str_to_vector<int>
-        case 'i':
+        if (std::is_same<T, int>::value)
+        {
             int start = 0, end = 0;
             while (start < input_string.size())
             {
@@ -29,10 +29,25 @@ namespace test_utils
                 output_vector.push_back(std::stoi(input_string.substr(start, end - start)));
                 start = end;
             }
-            break;
-
         }
+
+        // // str_to_vector<char>
+        // else if (std::is_same<T, char>::value)
+        // {
+
+        //     // check the type of quote used to enclose the element in the input string
+        //     char quote = input_string.find('\'') == std::string::npos ? '\"' : '\'';
+
+        //     int start = 0, end = 0;
+        //     while (end != std::string::npos)
+        //     {
+        //         start = input_string.find(quote, end + 1); // the first 
+        //         output_vector.push_back(input_string[start + 1]);
+        //         end = input_string.find(quote, start + 1);
+        //     }
+        // }
 
         return output_vector;
     }
+
 } // namespace test_utils
--- test_utils.h
+++ test_utils.h
@@ -7,16 +7,19 @@
 namespace test_utils
 {
     template<class T>
-    std::vector<T> str_to_vector(std::string, char);
+    std::vector<T> str_to_vector(std::string);
 
     template<class T>
-    std::vector<std::vector<T>>str_to_vector_2D(std::string, char);
+    std::vector<std::vector<T>>str_to_vector_2D(std::string);
 
     template<class T>
     std::string vector_to_str(std::vector<T>);
 
     template<class T>
     std::string vector_to_str_2D(std::string);
+
+    template<class T>
+    void print_vector(std::vector<T>);
 
 
 }; // namespace test_utils
