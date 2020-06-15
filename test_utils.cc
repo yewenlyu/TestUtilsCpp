55d1fb4db025b5b6fc7ad166a63ac326848bb683
yewenlyu2019 <46354696+yewenlyu2019@users.noreply.github.com>
2020-06-15 10:37:13 -0700

    implement str_to_vector<char>

--- test_utils.cc
+++ test_utils.cc
@@ -31,21 +31,23 @@ namespace test_utils
             }
         }
 
-        // // str_to_vector<char>
-        // else if (std::is_same<T, char>::value)
-        // {
+        // str_to_vector<char>
+        else if (std::is_same<T, char>::value)
+        {
+            // check the type of quote used to enclose the element in the input string
+            char quote = input_string.find('\'') == std::string::npos ? '\"' : '\'';
 
-        //     // check the type of quote used to enclose the element in the input string
-        //     char quote = input_string.find('\'') == std::string::npos ? '\"' : '\'';
+            int pos = 0;
+            pos = input_string.find(quote, 0);
+            while (pos != std::string::npos)
+            {
+                output_vector.push_back(input_string[pos + 1]);
 
-        //     int start = 0, end = 0;
-        //     while (end != std::string::npos)
-        //     {
-        //         start = input_string.find(quote, end + 1); // the first 
-        //         output_vector.push_back(input_string[start + 1]);
-        //         end = input_string.find(quote, start + 1);
-        //     }
-        // }
+                // find every other quote 
+                pos = input_string.find(quote, pos + 1);
+                pos = input_string.find(quote, pos + 1);
+            }
+        }
 
         return output_vector;
     }
